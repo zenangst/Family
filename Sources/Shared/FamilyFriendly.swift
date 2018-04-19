b390e5fab508cf0b653f8c94029dcc341ec92e7e
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-04-19 13:13:32 +0200

    Improve using observers for view controllers on iOS and tvOS

--- Sources/Shared/FamilyFriendly.swift
+++ Sources/Shared/FamilyFriendly.swift
@@ -2,8 +2,6 @@ import CoreGraphics
 import Foundation
 
 protocol FamilyFriendly: class {
-  var registry: [ViewController: View] { get set }
-
   func addChildViewController(_ childController: ViewController)
   func addChildViewController(_ childController: ViewController, customSpacing: CGFloat?, height: CGFloat)
   func addChildViewController<T: ViewController>(_ childController: T, customSpacing: CGFloat?, view closure: (T) -> View)
