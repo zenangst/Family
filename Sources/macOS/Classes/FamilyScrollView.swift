0c16978e888b5895657fee81930426ca8295cdf8
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-12-10 21:29:04 +0100

    Improve constrains in new algorithm

--- Sources/macOS/Classes/FamilyScrollView.swift
+++ Sources/macOS/Classes/FamilyScrollView.swift
@@ -309,13 +309,16 @@ public class FamilyScrollView: NSScrollView {
           newHeight = fmin(contentView.frame.height, scrollView.contentSize.height)
         }
 
-        if self.contentOffset.y <= entry.contentSize.height {
+        // Only scroll if the views content offset is less than its content size height
+        // and if the frame is less than the content size height.
+        let shouldScroll = contentOffset.y <= entry.contentSize.height &&
+          frame.size.height < entry.contentSize.height
+
+        if shouldScroll {
           scrollView.contentView.scroll(contentOffset)
           scrollView.frame.origin.y = frame.origin.y
-          if scrollView.frame.size.height != newHeight {
-            scrollView.frame.size.height = newHeight
-          }
-        } else if scrollView.frame.origin.y != entry.origin.y {
+          scrollView.frame.size.height = newHeight
+        } else {
           scrollView.frame.origin.y = entry.origin.y
         }
       }
