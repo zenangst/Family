0b5f6a64b33352ca7c7dd2a207360a23c344e9e9
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-04-23 13:13:34 +0200

    Update FamilyClipView.swift
    Use `.isScrollingByProxy` on `FamilyClipView`.
    
    Fixes rendering issues when views like `NSCollectionView` wants to scroll to a new position.

--- Sources/macOS/Classes/FamilyClipView.swift
+++ Sources/macOS/Classes/FamilyClipView.swift
@@ -1,14 +1,13 @@
 import Cocoa
 
 class FamilyClipView: NSClipView {
+  var wrapperView: FamilyWrapperView? { return enclosingScrollView as? FamilyWrapperView }
+  var scrollView: FamilyScrollView? { return wrapperView?.enclosingScrollView as? FamilyScrollView }
 
   override func scroll(to newOrigin: NSPoint) {
     super.scroll(to: newOrigin)
-    guard let wrapperView = enclosingScrollView as? FamilyWrapperView,
-      let familyScrollView = wrapperView.enclosingScrollView as? FamilyScrollView else {
-        return
-    }
-
+    guard let wrapperView = wrapperView, let familyScrollView = scrollView else { return }
+    familyScrollView.isScrollingByProxy = true
     familyScrollView.scrollTo(newOrigin, in: documentView!)
   }
 }
