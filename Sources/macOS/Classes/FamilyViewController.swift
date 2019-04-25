8e9ee8bcd8120faadc9c67061da0562341903d6e
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-04-25 21:19:28 +0200

    Fix retain cycle in macOS implementation
    `NSEvent.addLocalMonitorForEvents` holds a strong reference to the scroll view. Using settings the reference to self as weak resolves the retain cycle and fixes the memory leak.

--- Sources/macOS/Classes/FamilyViewController.swift
+++ Sources/macOS/Classes/FamilyViewController.swift
@@ -37,7 +37,6 @@ open class FamilyViewController: NSViewController, FamilyFriendly {
     scrollView.isDeallocating = true
     children.forEach { $0.removeFromParent() }
     purgeRemovedViews()
-
     if let eventHandlerKeyDown = eventHandlerKeyDown { NSEvent.removeMonitor(eventHandlerKeyDown) }
   }
 
@@ -64,8 +63,8 @@ open class FamilyViewController: NSViewController, FamilyFriendly {
       scrollView.isScrollEnabled = false
       scrollView.frame = view.bounds
     }
-    eventHandlerKeyDown = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { (event) -> NSEvent? in
-      self.scrollView.isScrollingByProxy = true
+    eventHandlerKeyDown = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { [weak self] event -> NSEvent? in
+      self?.scrollView.isScrollingByProxy = true
       return event
     }
   }
