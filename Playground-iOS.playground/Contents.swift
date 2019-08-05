d267f52e9700fc853c43d9c4d000a03fc87e4137
Christoffer Winterkvist <christoffer@winterkvist.com>
2019-08-05 15:19:19 +0200

    Fix playground not working
    Fixes #105

--- Playground-iOS.playground/Contents.swift
+++ Playground-iOS.playground/Contents.swift
@@ -5,14 +5,18 @@ import PlaygroundSupport
 let frame = CGRect(origin: .zero, size: CGSize(width: 320, height: 640))
 let window = UIWindow(frame: frame)
 let familyViewController = FamilyViewController()
+
 familyViewController.view.frame = window.bounds
 familyViewController.view.backgroundColor = .white
 
-let redViewController = UIViewController()
-redViewController.view.backgroundColor = UIColor.red.withAlphaComponent(0.2)
+PlaygroundPage.current.liveView = familyViewController
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 let blueViewController = UIViewController()
 blueViewController.view.backgroundColor = UIColor.blue.withAlphaComponent(0.2)
+
+let redViewController = UIViewController()
+redViewController.view.backgroundColor = UIColor.red.withAlphaComponent(0.2)
 
 let greenViewController = UIViewController()
 greenViewController.view.backgroundColor = UIColor.green.withAlphaComponent(0.2)
@@ -20,14 +24,9 @@ greenViewController.view.backgroundColor = UIColor.green.withAlphaComponent(0.2)
 let yellowViewController = UIViewController()
 yellowViewController.view.backgroundColor = UIColor.yellow.withAlphaComponent(0.2)
 
-familyViewController.addChildViewController(blueViewController, height: 200)
-familyViewController.addChildViewController(redViewController, height: 200)
-familyViewController.addChildViewController(greenViewController, height: 200)
-familyViewController.addChildViewController(yellowViewController, height: 200)
-
 window.rootViewController = familyViewController
 
-PlaygroundPage.current.liveView = familyViewController
-PlaygroundPage.current.needsIndefiniteExecution = true
-
-
+familyViewController.addChild(blueViewController, height: 200)
+familyViewController.addChild(redViewController, height: 200)
+familyViewController.addChild(greenViewController, height: 200)
+familyViewController.addChild(yellowViewController, height: 200)
--- Playground-macOS.playground/Contents.swift
+++ Playground-macOS.playground/Contents.swift
@@ -13,14 +13,23 @@ class CodeViewController: NSViewController {
 }
 
 let frame = NSRect(origin: .zero, size: CGSize(width: 320, height: 640))
+let familyViewController = FamilyViewController.init(nibName: nil, bundle: nil)
+let blueViewController = CodeViewController.init(nibName: nil, bundle: nil)
 let redViewController = CodeViewController.init(nibName: nil, bundle: nil)
+let greenViewController = CodeViewController.init(nibName: nil, bundle: nil)
+let yellowViewController = CodeViewController.init(nibName: nil, bundle: nil)
+
+familyViewController.view.setFrameSize(frame.size)
+
+blueViewController.view.layer?.backgroundColor = NSColor.blue.withAlphaComponent(0.2).cgColor
 redViewController.view.layer?.backgroundColor = NSColor.red.withAlphaComponent(0.2).cgColor
-redViewController.view.setFrameSize(frame.size)
+greenViewController.view.layer?.backgroundColor = NSColor.green.withAlphaComponent(0.2).cgColor
+yellowViewController.view.layer?.backgroundColor = NSColor.yellow.withAlphaComponent(0.2).cgColor
 
-//let familyController = FamilyViewController.init(nibName: nil, bundle: nil)
-//familyController.view.setFrameSize(frame.size)
-//familyController.addChildViewController(redViewController)
+familyViewController.addChild(blueViewController, height: 200)
+familyViewController.addChild(redViewController, height: 200)
+familyViewController.addChild(greenViewController, height: 200)
+familyViewController.addChild(yellowViewController, height: 200)
 
-
-PlaygroundPage.current.liveView = redViewController.view
+PlaygroundPage.current.liveView = familyViewController.view
 PlaygroundPage.current.needsIndefiniteExecution = true
